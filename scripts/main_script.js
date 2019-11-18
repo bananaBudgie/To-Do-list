7de42a5dfa4313659b1d1d68c6fd774df65e34cf
bananaBudgie <NicolaiMadsen91@protonmail.com>
2019-11-18 22:33:22 +0100

    Added buttons & misc
    Added 'check/done' & 'delete' buttons.
    Comments added to script.

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,7 +1,9 @@
-var taskList = ['[Auto-generated task_01] ', '[Auto-generated task_02] ', '[Auto-generated task_03]  ', '[Auto-generated task_04] ',]
+// Array - auto-genererer 4 tasks til 'li'
+var taskList = ['[Auto-generated task_01] ', '[Auto-generated task_02] ', '[Auto-generated task_03]  ', '[Auto-generated task_04] ', ]
 listTasks();
 
 function listTasks() {
+  // Sletter indhold der måtte være i 'taskList' og tilføjer på ny
   document.getElementById('tskList').innerHTML = '';
   for (var i = 0; i < taskList.length; i++) {
     addTaskElement(i);
@@ -9,18 +11,48 @@ function listTasks() {
   console.log(taskList);
 }
 
+// Funktion tilføjer tekst fra 'textInput' til 'li'
 function addNewTask() {
-  var taskName = document.getElementById('taskName').value;
+  var taskName = document.getElementById('textInput').value;
   var tsk = taskName;
   taskList.push(tsk);
   listTasks();
 }
-
+// Funktion tilføjer nye elementer til 'li'
 function addTaskElement(id) {
-  var newTsk = document.createElement('li');
+  // Tilføjer 'Checked' knap
+  var btnCheck = document.createElement('button');
+  var btnCheckText = document.createTextNode('✔')
+  // Tilføjer 'Delete' knap
+  var btnDelete = document.createElement('button');
+  var btnDeleteText = document.createTextNode('X')
+  // Tilføjer input fra textboks til 'li'
+  var newlyAddedTask = document.createElement('li');
   var node = document.createTextNode(taskList[id]);
-  newTsk.appendChild(node);
-  newTsk.setAttribute('id', 'task' + id);
-  newTsk.setAttribute('class', 'taskItem');
-  document.getElementById('tskList').append(newTsk);
+  // Tilføjer tekst til 'Checked' knap
+  btnCheck.appendChild(btnCheckText);
+  btnDelete.appendChild(btnDeleteText);
+  // Tilføjer tekst fra tekstboks til 'li'
+  newlyAddedTask.appendChild(node);
+  // Tilføjer tekst til 'Delete' knap
+  newlyAddedTask.appendChild(btnCheck);
+  newlyAddedTask.appendChild(btnDelete);
+  // Tilføjer id + class til nyt 'li' element
+  newlyAddedTask.setAttribute('id', 'task' + id);
+  newlyAddedTask.setAttribute('class', 'taskItem');
+  document.getElementById('tskList').append(newlyAddedTask);
+  // Tilføjer "event/click/listener" til 'Delete' knappen
+  btnDelete.addEventListener('click', function () {
+    deleteElement(id);
+  });
+  // Tilføjer "event/click/listener" til 'Checked' knappen
+  btnCheck.addEventListener('click', function () {
+    newlyAddedTask.style.textDecoration = "line-through";
+  });
+
+}
+//Funktion til overstående 'Delete' knap
+function deleteElement(id) {
+  taskList.splice(id, 1);
+  listTasks();
 }
